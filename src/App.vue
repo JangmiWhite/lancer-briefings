766f44275cee85de615634058d5820b155c873cd
JangmiWhite <144067570+JangmiWhite@users.noreply.github.com>
2023-09-04 18:24:35 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -102,7 +102,7 @@ export default {
       "pilots": [
         {
           "callsign": "Cerberus",
-          "player": "RIP",
+          "alias": "RIP",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
@@ -110,7 +110,7 @@ export default {
         },
         {
           "callsign": "Euryale",
-          "player": "Alice",
+          "alias": "Alice",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
@@ -118,7 +118,7 @@ export default {
         },
         {
           "callsign": "Patchwork",
-          "player": "Marble",
+          "alias": "Marble",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Chomolungma",
@@ -126,7 +126,7 @@ export default {
         },
         {
           "callsign": "Shallows",
-          "player": "Wurst",
+          "alias": "Wurst",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
           "frame": "Everest",
@@ -134,7 +134,7 @@ export default {
         },
         {
           "callsign": "Zee",
-          "player": "Disador",
+          "alias": "Disador",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Sagarmatha",
