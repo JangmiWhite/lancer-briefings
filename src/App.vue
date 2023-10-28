1bdca1fa974e8954f1251d867eecc09319e27490
JangmiWhite <144067570+JangmiWhite@users.noreply.github.com>
2023-10-28 16:05:02 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -125,7 +125,7 @@ export default {
           "mech": "Monsoon"
         },
         {
-          "callsign": "Mellifera",
+          "callsign": "Shallows",
           "alias": "Wurst",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "HORUS",
